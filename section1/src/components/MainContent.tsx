d34128da27ea241655d678950ecd549b61008981
Matt-Gaston <19mattgaston@gmail.com>
2025-09-12 17:06:50 -0500

    built a simple static page. I hate css

---
+++ section1/src/components/MainContent.tsx
@@ -0,0 +1,16 @@
+function MainContent() {
+    return(
+        <main>
+            <h1>Fun Facts about react</h1>
+            <ul className="factslist">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
+
+export default MainContent;
