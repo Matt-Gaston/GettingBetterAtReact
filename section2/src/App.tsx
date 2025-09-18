b7bb13d82f643611c79003711d033ada6d2e434a
Matt-Gaston <19mattgaston@gmail.com>
2025-09-18 13:50:03 -0500

    section 2. Header bar added

---
+++ section2/src/App.tsx
@@ -0,0 +1,10 @@
+import HeaderBar from "./components/HeaderBar/HeaderBar";
+import './App.css';
+
+function App() {
+    return (
+        <HeaderBar />
+    )
+}
+
+export default App;
---
+++ section2/src/components/HeaderBar/HeaderBar.tsx
@@ -0,0 +1,12 @@
+import './HeaderBar.css';
+
+function HeaderBar() {
+    return(
+        <header>
+            <img src='/src/assets/globe.png' alt="Globe icon"/>
+            <span>my travel journal</span>
+        </header>
+    )
+}
+
+export default HeaderBar;
