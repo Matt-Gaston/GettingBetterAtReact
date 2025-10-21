32d507196ad8098d14a4637d165311aec5a1b890
MattGaston <19mattgaston@gmail.com>
2025-10-20 23:49:23 -0500

    finished up, added random img loading

--- section4/src/components/MemeGen/MemeGen.tsx
+++ section4/src/components/MemeGen/MemeGen.tsx
@@ -1,7 +1,52 @@
+import { useState, useEffect } from "react";
 import "./MemeGen.css"
 
 
+
 function MemeGen() {
+    type ImgFlipMeme = {
+        box_count: number,
+        captions: number,
+        height: number,
+        id: string,
+        name: string,
+        url: string,
+        width: number
+    }
+
+    const [meme, setMeme] = useState({
+        topText:"One does not simply",
+        bottomText:"Walk into Mordor",
+        image_src: "http://i.imgflip.com/1bij.jpg"})
+
+    const [allMemes, setAllMemes] = useState<ImgFlipMeme[]>([])
+
+    
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then( (res) => res.json() )
+            .then( (data) => setAllMemes(data.data.memes))
+    }, [])
+
+    function handleTextChange (event: React.ChangeEvent<HTMLInputElement>){
+        const {value, name} = event.currentTarget
+        setMeme((prev_meme) => ({
+            ...prev_meme,
+            [name]: value
+        }))
+    }
+
+
+    function randomChangeMemeImg () {
+        const image_index = Math.floor(Math.random() * allMemes.length);
+        const image_url = allMemes[image_index].url
+        setMeme( (prev_meme) => ({
+            ...prev_meme,
+            image_src: image_url
+        }
+        ))
+    }
+
     return (
         <main>
             <div className="form">
@@ -10,6 +55,8 @@ function MemeGen() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleTextChange}
+                        value={meme.topText}
                     />
                 </label>
 
@@ -18,14 +65,16 @@ function MemeGen() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleTextChange}
+                        value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={randomChangeMemeImg}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.image_src} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
