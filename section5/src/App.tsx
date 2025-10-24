4155ac1394913f5a7822a06f0f19a748e779ebef
MattGaston <19mattgaston@gmail.com>
2025-10-24 14:09:56 -0500

    added dice randomization

--- section5/src/App.tsx
+++ section5/src/App.tsx
@@ -1,20 +1,26 @@
 import "./App.css";
 import Dice from "./Dice/Dice.tsx";
+import { useState } from "react";
 
 function App() {
+
+    const [dieValues, setDieValues] = useState<number[]>(generateAllnewDice())
+
+    function generateAllnewDice(): number[]{
+        return Array(10)
+            .fill(0)
+            .map(() => Math.floor(Math.random()*6 +1))
+    }
+
+    const diceElements = dieValues.map(
+                    (val) => <Dice value={val}/>
+                )
+
+    console.log(generateAllnewDice())
     return (
         <main>
             <div className="dieContainer">
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
+                {diceElements}
             </div>
         </main>
     )
