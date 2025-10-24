4d4c4aa8b59412322821c16598ef6f22b9f5e779
MattGaston <19mattgaston@gmail.com>
2025-10-24 16:00:14 -0500

    finished up dice game

--- section5/src/App.tsx
+++ section5/src/App.tsx
@@ -1,27 +1,77 @@
 import "./App.css";
 import Dice from "./Dice/Dice.tsx";
 import { useState } from "react";
+import { nanoid } from "nanoid";
+import Confetii from "react-confetti";
 
 function App() {
 
-    const [dieValues, setDieValues] = useState<number[]>(generateAllnewDice())
+    const [dieValues, setDieValues] = useState(() => generateAllnewDice())
+    const diceElements = dieValues.map(
+                    (val) => <Dice 
+                        value={val.value}
+                        isHeld={val.isHeld}
+                        key={val.id}
+                        holdFunc={() => swapDiceHoldState(val.id)}
+                        />
+                )
+    const gameWon = (dieValues.every((die) => die.isHeld) && 
+                    dieValues.every((die) => die.value === dieValues[0].value))
 
-    function generateAllnewDice(): number[]{
+    function generateAllnewDice() {
         return Array(10)
             .fill(0)
-            .map(() => Math.floor(Math.random()*6 +1))
+            .map(() => ({
+                value: Math.floor(Math.random()*6 +1),
+                isHeld: false,
+                id: nanoid()}
+            ))
     }
 
-    const diceElements = dieValues.map(
-                    (val) => <Dice value={val}/>
-                )
+    function rerollDice(){
+        if (gameWon) {
+            setDieValues((prev_dieState) => (
+                prev_dieState.map((die) => (
+                    {
+                        value: Math.floor(Math.random()*6 +1),
+                        isHeld: false,
+                        id: die.id
+                    }
+                ))
+            ))
+        } else {
+            setDieValues((prev_dieState) => (
+                prev_dieState.map((die) => (
+                    {
+                        ...die,
+                        value: die.isHeld ? die.value : Math.floor(Math.random()*6 +1)
+                    }
+                ))
+            ))
+        }
+    }
 
-    console.log(generateAllnewDice())
+    function swapDiceHoldState(id:string) {
+        setDieValues((prev_dieState) => (
+            prev_dieState.map((die) => (
+                {
+                    ...die,
+                    isHeld: die.id === id ? !die.isHeld : die.isHeld
+                }
+            ))
+        ))
+    }
+    
+
     return (
         <main>
+            {gameWon ? <Confetii/> : null}
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dieContainer">
                 {diceElements}
             </div>
+            <button className="roll-dice-btn" onClick={rerollDice}>{gameWon ? "New Game" : "Roll"}</button>
         </main>
     )
 }
--- section5/src/Dice/Dice.tsx
+++ section5/src/Dice/Dice.tsx
@@ -2,11 +2,17 @@ import "./Dice.css";
 
 interface DiceProps {
     value: number;
+    isHeld: boolean;
+    holdFunc: () => void;
 }
 
 function Dice(props: DiceProps) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#e9e9ed"
+    }
+
     return (
-        <button>{props.value}</button>
+        <button onClick={props.holdFunc} style={styles}>{props.value}</button>
     )
 }
 
